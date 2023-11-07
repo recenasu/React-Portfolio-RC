b92c6aa20093702a599628d7fb52b5ea422fe3bf
Domitius <133902698+recenasu@users.noreply.github.com>
2023-11-06 17:56:32 -0700

    added nav button hover and active effects

--- React-Portfolio-RC/src/components/Navigation.jsx
+++ React-Portfolio-RC/src/components/Navigation.jsx
@@ -1,4 +1,6 @@
 // This is the navigation bar component of the web page.
+// Import react Link component for link navigation
+import { NavLink } from 'react-router-dom';
 
 const styles = {
     navButtonsContainer: {
@@ -16,6 +18,13 @@ const styles = {
         justifyContent: 'space-around',
         alignItems: 'center'
     },
+    listItem: {
+      width: 120,
+      height: 40,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
 };
 
 function Navigation() {
@@ -23,10 +32,44 @@ function Navigation() {
         <div style={styles.navButtonsContainer}>
             <ul style={{ listStyleType: 'none', width: '100%' }}>
                 <div style={styles.flexRow}>
-                    <li>ABOUT ME</li>
-                    <li>PORTFOLIO</li>
-                    <li>CONTACT</li>
-                    <li>RESUME</li>
+                    <NavLink key={1}
+                    to='/'
+                    style={({ isActive }) => {
+                        return {
+                            background: isActive ? '#F7FAE3' : '',
+                            color: isActive ? 'blue' : '',
+                        }
+                    }}
+                    className="nav_link" >
+                        <li style={styles.listItem}>ABOUT ME</li>
+                    </NavLink>
+                    <NavLink key={2} to='portfolio' style={({ isActive }) => {
+                        return {
+                            background: isActive ? '#F7FAE3' : '',
+                            color: isActive ? 'blue' : '',
+                        }
+                    }}
+                    className="nav_link">
+                        <li style={styles.listItem}>PORTFOLIO</li>
+                    </NavLink>
+                    <NavLink key={3} to='contact' style={({ isActive }) => {
+                        return {
+                            background: isActive ? '#F7FAE3' : '',
+                            color: isActive ? 'blue' : '',
+                        }
+                    }}
+                    className="nav_link">
+                        <li style={styles.listItem}>CONTACT</li>
+                    </NavLink>
+                    <NavLink key={4} to='resume' style={({ isActive }) => {
+                        return {
+                            background: isActive ? '#F7FAE3' : '',
+                            color: isActive ? 'blue' : '',
+                        }
+                    }}
+                    className="nav_link">
+                        <li style={styles.listItem}>RESUME</li>
+                    </NavLink>
                 </div>
             </ul>
         </div>
--- React-Portfolio-RC/src/pages/PortfolioPage.jsx
+++ React-Portfolio-RC/src/pages/PortfolioPage.jsx
@@ -1,7 +1,10 @@
 // This is the Portfolio page, displayed when the Portfolio option is selected in the Nav bar.
+// Import react Link component for link navigation
+import { Link } from 'react-router-dom';
+import Project from '../components/Project';
 
 export default function PortfolioPage() {
     return (
-    <p>This is the Portfolio Page</p>
+    <Project />
     );
 };
