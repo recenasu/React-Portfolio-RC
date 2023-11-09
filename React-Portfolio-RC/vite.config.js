01d0d98916c59c909440a44eb38967b99c3a1b95
Domitius <133902698+recenasu@users.noreply.github.com>
2023-11-09 00:49:53 -0700

    netlify debug

--- React-Portfolio-RC/vite.config.js
+++ React-Portfolio-RC/vite.config.js
@@ -4,8 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx',
+      },
+    },
+  },
+});
+
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
