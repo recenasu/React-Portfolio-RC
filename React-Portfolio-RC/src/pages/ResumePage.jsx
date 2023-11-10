139956900a56dbb23e3793296192d4b96884086f
Domitius <133902698+recenasu@users.noreply.github.com>
2023-11-09 22:48:53 -0700

    debug

--- React-Portfolio-RC/src/pages/ResumePage.jsx
+++ React-Portfolio-RC/src/pages/ResumePage.jsx
@@ -1,4 +1,6 @@
 // This is the Resume page, displayed when the Resume option is selected in the Nav bar.
+import { Link } from 'react-router-dom';
+import resumeFile from '../assets/rick_test_resume.pdf';
 
 const styles = {
     justifyText: {
@@ -14,7 +16,7 @@ export default function ResumePage() {
     return (
         <div>
             <h2>Rick's Resume </h2>
-            <a href="./src/assets/rick_test_resume.pdf" download><button>Download pdf</button></a>
+            <a href={resumeFile} download><button>Download pdf</button></a>
             <ul style={styles.justifyText}>
                 <li>Rick knows a little JavaScript, HTML, CSS, Node.js, React.js, Handlebars.js, Express.js, MySQL, and MongoDB</li>
                 <li>This is something Rick did</li>
